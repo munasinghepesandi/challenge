cabaf0f4fe648dfc0b4cef9972f3823a0512f342
Pesandi Pabasara Munasinghe <munasinghepesandi@gmail.com>
2024-11-15 13:13:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import Hero from "./Hero";
+import Navigation from "./Navigation";
+
+function App() {
+  const name = "Manupa";
+  const cartCount = 2;
+
+  return (
+    <div>
+      <Navigation name={name} cartCount={cartCount} />
+      <Hero />
+      <div className="p-4">
+        <Button varient="outline">Buy Now</Button>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
